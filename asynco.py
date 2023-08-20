2779c45b7ae0817e4ffb93ac4d85291702d8501c
Unit-91 <djangodev2022@gmail.com>
2023-08-20 17:00:53 +0600

    asyncio on callbacks

---
+++ asynco.py
@@ -0,0 +1,57 @@
+import time
+from collections import deque
+import heapq
+
+
+class Scheduler:
+    def __init__(self):
+        self.ready = deque()    # Functions ready to execute
+        self.sleeping = []  # Sleeping functions
+        self.sequence = 0
+
+    def call_soon(self, func):
+        self.ready.append(func)
+
+    def call_later(self, delay, func):
+        self.sequence += 1
+        deadline = time.time() + delay  # Expiration time
+        # Priority queue
+        heapq.heappush(self.sleeping, (deadline, self.sequence, func))
+
+    def run(self):
+        while self.ready or self.sleeping:
+            if not self.ready:
+                # Find the nearest deadline
+                deadline, _, func = heapq.heappop(self.sleeping)
+                delta = deadline - time.time()
+                if delta > 0:
+                    time.sleep(delta)
+                self.ready.append(func)
+
+            while self.ready:
+                func = self.ready.popleft()
+                func()
+
+
+sched = Scheduler()
+
+
+def count_down(n):
+    if n > 0:
+        print('Down', n)
+        # time.sleep(4)   # Blocking call (nothing else can run)
+        sched.call_later(4, lambda: count_down(n - 1))
+
+
+def count_up(stop):
+    def _run(x):
+        if x < stop:
+            print('Up', x)
+            # time.sleep(1)
+            sched.call_later(1, lambda: _run(x + 1))
+    _run(0)
+
+
+sched.call_soon(lambda: count_down(5))
+sched.call_soon(lambda: count_up(20))
+sched.run()
