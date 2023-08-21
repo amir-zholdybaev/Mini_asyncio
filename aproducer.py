fc8063f2196c73b2f391c1540e741a8689087be5
Unit-91 <djangodev2022@gmail.com>
2023-08-21 19:53:42 +0600

    moved the scheduler to a separate file scheduler.py

--- aproducer.py
+++ aproducer.py
@@ -3,79 +3,10 @@
 # Async Producer-consumer problem.
 # Challenge: How to implement the same functionality, but no threads.
 
-import time
 from collections import deque
-import heapq
-
-
-class Scheduler:
-    def __init__(self):
-        self.ready = deque()     # Functions ready to execute
-        self.sleeping = []       # Sleeping functions
-        self.sequence = 0
-
-    def call_soon(self, func):
-        self.ready.append(func)
-
-    def call_later(self, delay, func):
-        self.sequence += 1
-        deadline = time.time() + delay     # Expiration time
-        # Priority queue
-        heapq.heappush(self.sleeping, (deadline, self.sequence, func))
-
-    def run(self):
-        while self.ready or self.sleeping:
-            if not self.ready:
-                # Find the nearest deadline
-                deadline, _, func = heapq.heappop(self.sleeping)
-                difference = deadline - time.time()
-                if difference > 0:
-                    time.sleep(difference)
-                self.ready.append(func)
-
-            while self.ready:
-                func = self.ready.popleft()
-                func()
-
+from scheduler import Scheduler
 
 sched = Scheduler()     # Behind scenes scheduler object
-
-
-"""
-    Scheduler - Решает какие функции нужно вызвать сразу, а какие вызвать позже в определенное время
-
-    call_soon - Добавляет функцию в очередь готовых к выполнению
-
-    call_later - Добавляет в очередь ждущих выполнения функцию вместе с временем ее вызова в будущем
-
-    run - Запускает выполнение функций
-
-    Запускает цикл, который выполняется пока в очереди готовых или ждущих что то есть
-
-        Если в очереди готовых ничего нет:
-
-            То из очереди ждущих, достает функцию, у которой самое ближайшее время вызова
-
-            Вычисляет оставшееся до вызова время
-
-            Если оно больше нуля, то делает паузу на это время, а после добавляет в очередь готовых
-
-            Если оно равно или меньше нуля, то сразу добавляет в очередь готовых
-
-        А после достает все функции из очереди готовых и вызывает
-
-        Если в очереди готовых что то есть, то сразу достает все функции из очереди готовых и вызывает
-
-        Цикл начинает новую итерацию
-
-    Простыми словами метод run:
-    Достает все фукнции из очереди готовых и вызывает их
-    Если в очереди готовых ничего нет, то достает ближайщую функцию из очереди ждущих,
-    добавляет в очередь готовых, достает оттуда и вызывает
-
-"""
-
-# ==========================================================================================
 
 
 class AsyncQueue:
--- asynco.py
+++ asynco.py
@@ -1,37 +1,4 @@
-import time
-from collections import deque
-import heapq
-
-
-class Scheduler:
-    def __init__(self):
-        self.ready = deque()    # Functions ready to execute
-        self.sleeping = []  # Sleeping functions
-        self.sequence = 0
-
-    def call_soon(self, func):
-        self.ready.append(func)
-
-    def call_later(self, delay, func):
-        self.sequence += 1
-        deadline = time.time() + delay  # Expiration time
-        # Priority queue
-        heapq.heappush(self.sleeping, (deadline, self.sequence, func))
-
-    def run(self):
-        while self.ready or self.sleeping:
-            if not self.ready:
-                # Find the nearest deadline
-                deadline, _, func = heapq.heappop(self.sleeping)
-                difference = deadline - time.time()
-                if difference > 0:
-                    time.sleep(difference)
-                self.ready.append(func)
-
-            while self.ready:
-                func = self.ready.popleft()
-                func()
-
+from scheduler import Scheduler
 
 sched = Scheduler()
 
@@ -55,38 +22,3 @@ def count_up(stop):
 sched.call_soon(lambda: count_down(5))
 sched.call_soon(lambda: count_up(20))
 sched.run()
-
-
-"""
-    Scheduler - Решает какие функции нужно вызвать сразу, а какие вызвать позже в определенное время
-
-    call_soon - Добавляет функцию в очередь готовых к выполнению
-
-    call_later - Добавляет в очередь ждущих выполнения функцию вместе с временем ее вызова в будущем
-
-    run - Запускает выполнение функций
-
-    Запускает цикл, который выполняется пока в очереди готовых или ждущих что то есть
-
-        Если в очереди готовых ничего нет:
-
-            То из очереди ждущих, достает функцию, у которой самое ближайшее время вызова
-
-            Вычисляет оставшееся до вызова время
-
-            Если оно больше нуля, то делает паузу на это время, а после добавляет в очередь готовых
-
-            Если оно равно или меньше нуля, то сразу добавляет в очередь готовых
-
-        А после достает все функции из очереди готовых и вызывает
-
-        Если в очереди готовых что то есть, то сразу достает все функции из очереди готовых и вызывает
-
-        Цикл начинает новую итерацию
-
-    Простыми словами метод run:
-    Достает все фукнции из очереди готовых и вызывает их
-    Если в очереди готовых ничего нет, то достает ближайщую функцию из очереди ждущих,
-    добавляет в очередь готовых, достает оттуда и вызывает
-
-"""
