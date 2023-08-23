84f75ae036d37438e4bf2233853450e3c04312c4
Unit-91 <djangodev2022@gmail.com>
2023-08-23 16:10:22 +0600

    Took out the producer's and consumer's code to a file async_producer_consumer_with_error.py

--- async_producer_consumer_with_error.py
+++ async_producer_consumer_with_error.py
@@ -1,9 +1,4 @@
-# aproducer.py
-#
-# Async Producer-consumer problem.
-# Challenge: How to implement the same functionality, but no threads.
-
-from async_queue import AsyncQueue, sched
+from async_queue_with_error import AsyncQueue, sched, QueueClosed
 
 aq = AsyncQueue()
 
@@ -16,7 +11,7 @@ def producer(q, count):
             sched.call_later(2, lambda: _run(n + 1))
         else:
             print('Producer done')
-            q.put(None)
+            q.close()   # Means no more items will be produced
 
     _run(0)
 
@@ -25,17 +20,19 @@ def producer(q, count):
     Производитель генерирует данные и кладет их в очередь и оповещает потребителя о положенных данных,
     если есть ждущий потребитель
     Делает он это каждые n секунд и до тех пор, пока не достигнет конца счетчика
-    Когда счетчик закончен, завершает свою работу
+    Когда счетчик закончен, завершает свою работу и закрывает очередь
 """
 
 
 def consumer(q):
-    def _consume(item):
-        if item is None:
-            print('Consumer done')
-        else:
+    def _consume(result):
+        try:
+            item = result.result()
             print('Consuming', item)
             sched.call_soon(lambda: consumer(q))
+
+        except QueueClosed:
+            print('Consumer done')
 
     q.get(callback=_consume)
 
@@ -45,7 +42,10 @@ def consumer(q):
     Если в очереди есть готовые данные, он берет их, обрабатывает и запрашивает по новой (sched.call_soon(lambda: consumer(q)))
     Если в очереди данных нет, он их ждет
     Повторяет он эти действия до тех пор, пока производитель данных не прекратит свою работу
+    Когда производитель завершает свою работу, то он закрывает очередь и потребитель вместо данных получает ошибку,
+    отлавливает ее и заканчивает работу
 """
+
 
 sched.call_soon(lambda: producer(aq, 10))
 sched.call_soon(lambda: consumer(aq,))
